44cdc7640bddc4d0360f668429fb7fa99d151e62
axeld5 <axel.darmouni@student-cs.fr>
2024-10-15 22:32:46 +0200

    prettying file

--- subtitle_video.py
+++ subtitle_video.py
@@ -3,8 +3,6 @@ from moviepy.video.VideoClip import TextClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-
-# Load the video file
 def subtitle_video(video_path:str, subtitle_path:str, output_path:str) -> None:
     video = VideoFileClip(video_path)
     generator = lambda txt: TextClip(txt, font='Comic-Sans-MS-Bold-Italic', fontsize=50, color='yellow', stroke_color='black', stroke_width=2)
